eb1abd10e092286130904f2da472691daea426ae
alejandromunozc <alejandromunozc@gmail.com>
2020-05-22 16:17:45 -0500

    Funciones con Prototipos

---
+++ clasesPrototipos.js
@@ -0,0 +1,20 @@
+function Persona(nombre, apellido, edad, altura) {
+    this.nombre = nombre;
+    this.apellido = apellido;
+    this.edad = edad;
+    this.altura = altura;
+}
+
+Persona.prototype.presentacion = function() {
+    console.log(`Hola, mi nombre es ${this.nombre} ${this.apellido}, tengo ${this.edad} años y mido ${this.altura} metros`);
+}
+
+Persona.prototype.cumpleannios = function() {
+    this.edad++;
+    console.log(`He cumplido años, ahora tengo ${this.edad} años.`);
+}
+
+var cristian = new Persona('Cristian', 'Muñoz', 34, 1.35);
+var magolo = new Persona('Magolo', 'Bautizta', 28, 1.60);
+var petunio = new Persona('Petunio', 'Florez', 14, 1.55);
+var florencio = new Persona('Florencio', 'Caparazon', 51, 1.80);
